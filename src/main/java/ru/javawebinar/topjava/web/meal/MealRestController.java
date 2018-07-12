dc56921236601be3b9d3c46eb091604a5213ee43
abuzuverov <andrey.buzuverov@gmail.com>
2018-07-12 22:11:36 +0300

    Meals layers refactoring

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,44 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
 public class MealRestController {
+
+    @Autowired
     private MealService service;
+
+    public void update(Meal meal, int id) {
+        assureIdConsistent(meal, id);
+        service.update(meal, SecurityUtil.authUserId());
+    }
+
+    public Meal create(Meal meal) {
+        checkNew(meal);
+        return service.create(meal, SecurityUtil.authUserId());
+    }
+
+    public void delete(int id) {
+        service.delete(id, SecurityUtil.authUserId());
+    }
+
+    public Meal get(int id) {
+        return service.get(id, SecurityUtil.authUserId());
+    }
+
+    public List<MealWithExceed> getAll() {
+        return MealsUtil.getWithExceeded(service.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay());
+    }
 
 }
