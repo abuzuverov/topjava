11777e5b10ac0d54a07263cb424fd22ca7e374e8
abuzuverov <andrey.buzuverov@gmail.com>
2018-07-28 20:54:39 +0300

    hw4

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepositoryImpl.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class JpaMealRepositoryImpl implements MealRepository {
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Override
     public Meal save(Meal meal, int userId) {
@@ -17,21 +26,38 @@ public class JpaMealRepositoryImpl implements MealRepository {
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+/*        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaDelete<Meal> cd = cb.createCriteriaDelete(Meal.class);
+        Root<Meal> meal = cd.from(Meal.class);
+        Predicate pId = cb.equal(meal.get("id"), id);
+        Predicate pUserId = cb.equal(meal.get("userId"), userId);
+        cd.where();
+        return false;*/
+        em.remove(get(id, userId));
+        return true;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return em.createQuery("select m from Meal m join fetch m.user u where m.id = :id and u.id = :userId", Meal.class)
+                .setParameter("id", id)
+                .setParameter("userId", userId)
+                .getSingleResult();
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return em.createQuery("select m from Meal m join fetch m.user u where u.id = :userId order by m.dateTime desc", Meal.class)
+                .setParameter("userId", userId)
+                .getResultList();
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return em.createQuery("select m from Meal m join fetch m.user u where u.id = :userId and m.dateTime between :startDate and :endDate order by m.dateTime desc", Meal.class)
+                .setParameter("userId", userId)
+                .setParameter("startDate", startDate)
+                .setParameter("endDate", endDate)
+                .getResultList();
     }
 }
