b90fa06d75e29a655f61e69bd20b8c3c2ab9a38b
abuzuverov <andrey.buzuverov@gmail.com>
2018-07-31 00:54:37 +0300

    DataJpaMealRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,21 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Transactional
+    int deleteByIdAndUserId(int id, int userId);
+
+    Meal getByIdAndUserId(int id, int userId);
+
+    List<Meal> findAllByUserIdOrderByDateTimeDesc(int userId);
+
+    List<Meal> findAllByUserIdAndDateTimeIsBetweenOrderByDateTimeDesc(int userId, LocalDateTime startDate, LocalDateTime endDate);
+
 }
--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,7 +1,9 @@
 package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealWithExceed;
@@ -14,16 +16,24 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+//@Profile({"postgres", "datajpa"})
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/mock.xml")) {
+        try (ConfigurableApplicationContext appCtx = new GenericXmlApplicationContext()) {
+            appCtx.getEnvironment().setActiveProfiles("postgres", "datajpa");
+            ((GenericXmlApplicationContext) appCtx).load("spring/spring-app.xml", "spring/spring-db.xml");
+            appCtx.refresh();
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+            /*AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
-            System.out.println();
+            System.out.println();*/
 
             MealRestController mealController = appCtx.getBean(MealRestController.class);
+            //mealController.getAll().forEach(System.out::println);
+            //System.out.println(mealController.get(MealTestData.MEAL1_ID));
+            //mealController.delete(MealTestData.MEAL1_ID);
+            //mealController.getAll().forEach(System.out::println);
             List<MealWithExceed> filteredMealsWithExceeded =
                     mealController.getBetween(
                             LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
