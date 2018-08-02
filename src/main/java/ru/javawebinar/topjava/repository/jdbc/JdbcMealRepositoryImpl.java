a97bdae4fc4e291c8ecac2105e623ac10b4ace42
abuzuverov <andrey.buzuverov@gmail.com>
2018-08-02 09:35:45 +0300

    hw5 opt

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -15,16 +15,17 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Profile("jdbc")
-public abstract class JdbcMealRepositoryImpl implements MealRepository {
+public abstract class JdbcMealRepositoryImpl<T> implements MealRepository {
 
-    protected static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
-
-    @Autowired
-    protected JdbcTemplate jdbcTemplate;
+    private static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
 
     @Autowired
-    protected NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    protected abstract T getTimestamp(LocalDateTime localDateTime);
 
     @Override
     public Meal save(Meal meal, int userId) {
@@ -32,7 +33,7 @@ public abstract class JdbcMealRepositoryImpl implements MealRepository {
                 .addValue("id", meal.getId())
                 .addValue("description", meal.getDescription())
                 .addValue("calories", meal.getCalories())
-                .addValue("date_time", meal.getDateTime())
+                .addValue("date_time", getTimestamp(meal.getDateTime()))
                 .addValue("user_id", userId);
 
         if (meal.isNew()) {
@@ -75,6 +76,6 @@ public abstract class JdbcMealRepositoryImpl implements MealRepository {
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
         return jdbcTemplate.query(
                 "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, startDate, endDate);
+                ROW_MAPPER, userId, getTimestamp(startDate), getTimestamp(endDate));
     }
 }
--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcPostgresMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcPostgresMealRepositoryImpl.java
@@ -2,8 +2,16 @@ package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
+
+import java.time.LocalDateTime;
 
 @Repository
-@Profile("postgres")
-public class JdbcPostgresMealRepositoryImpl extends JdbcMealRepositoryImpl {
+@Profile(Profiles.POSTGRES_DB)
+public class JdbcPostgresMealRepositoryImpl extends JdbcMealRepositoryImpl<LocalDateTime> {
+
+    @Override
+    protected LocalDateTime getTimestamp(LocalDateTime localDateTime) {
+        return localDateTime;
+    }
 }
